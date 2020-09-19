3584fc910d4145d5620c41d77020770a5fd9faf1
zamaseko <zamaseko@student.wethinkcode.co.za>
2020-09-19 21:42:53 +0200

    create and write

--- launcher/MayDay.java
+++ launcher/MayDay.java
@@ -1,35 +1,70 @@
 package launcher;
 
-import java.io.*;
+// import java.io.*;
+// import java.lang.*;
+import java.util.*;
 
-public class MayDay {
-    public void createFile() {
-        File cf = new File("stimulation.txt");
+public class MayDay{
+    private Formatter x;
+
+    public void createFile()
+    {
         try
         {
-           // cf.delete();
-            cf.createNewFile();
+            x = new Formatter("stimulator.txt");
         }
-        catch (Exception e) 
+        catch(Exception e)
         {
-            System.out.println("The record log was not successfully established");
+            System.out.println("stimulator.txt was not created okkkrrr");
         }
     }
-
     public void writeRecord(String s)
     {
-       // FileWriter wr;
         try
         {
-            FileWriter wr = new FileWriter("stimulation.txt", true);
-            wr.write("%s\n");
-            wr.close();
+            x.format("%s\n");
         }
-        catch (IOException e) 
+        catch(Exception e)
         {
-            e.printStackTrace();
+            System.out.println("stimulator.txt was not populated Okkkrrr");;
         }
     }
+    public void closeFile()
+    {
+        x.close(); 
+    }
+
 }
+// import java.io.*;
+
+// public class MayDay {
+//     public void createFile() {
+//         File cf = new File("stimulation.txt");
+//         try
+//         {
+//            // cf.delete();
+//             cf.createNewFile();
+//         }
+//         catch (Exception e) 
+//         {
+//             System.out.println("The record log was not successfully established");
+//         }
+//     }
+
+//     public void writeRecord(String s)
+//     {
+//        // FileWriter wr;
+//         try
+//         {
+//             FileWriter wr = new FileWriter("stimulation.txt", true);
+//             wr.write("%s\n");
+//             wr.close();
+//         }
+//         catch (IOException e) 
+//         {
+//             e.printStackTrace();
+//         }
+//     }
+// }
 
         
