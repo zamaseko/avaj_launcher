3e8f743c44bdb3224124756e616c029f165bc9bf
zamaseko <zamaseko@student.wethinkcode.co.za>
2020-09-19 23:35:54 +0200

    Stimulator

--- launcher/Stimulator.java
+++ launcher/Stimulator.java
@@ -4,18 +4,9 @@ import java.util.*;
 import java.io.*;
 
 public class Stimulator {
-    public static void main(String[] args) {
-        List<String> in = new ArrayList<>();
-        try 
-        {
-            File f = new File(args[0]);
-            Scanner scan = new Scanner(f);
-            MayDay may = new MayDay();
-        }
-        catch (FileNotFoundException e) 
-        {
-            e.printStackTrace();
-        }
-        
+    public static void main(String[] args)
+    {
+        if (args.length < 1)
+            System.out.println("Error Error");
     }
 }
