4d5e57a15c7a538e77160848c37783b3bfe69882
zamaseko <zamaseko@student.wethinkcode.co.za>
2020-09-25 21:06:48 +0200

    hello

--- launcher/AircraftFactory.java
+++ launcher/AircraftFactory.java
@@ -5,8 +5,8 @@ public abstract class AircraftFactory
     public static Flyable newAircraft(String type, String name, int longitude, int latitude, int height)
     {
         Coordinates co = new Coordinates(longitude, latitude, height);
-        // if(type != null)
-        // {
+        if(type != null)
+        {
             switch(type)
             {
                 case "Baloon":
@@ -19,7 +19,7 @@ public abstract class AircraftFactory
                     JetPlane jp = new JetPlane(name, co);
                     return jp;
             }
-        // }
+        }
         return null;
     }
 }
